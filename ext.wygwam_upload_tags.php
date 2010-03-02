db9f0dc9c3f80ccffe8b74b59f4f784d5f0dc940
Erik Reagan <erik@erikreagan.com>
2010-03-02 14:15:59 -0500

    Converted to EE 1.x compatible

--- ext.wygwam_upload_tags.php
+++ ext.wygwam_upload_tags.php
@@ -1,15 +1,16 @@
 <?php if ( ! defined('APP_VER')) exit('No direct script access allowed');
 
-
 /**
  * Wygwam Upload Tags
  * 
  * @author    Brandon Kelly <brandon@pixelandtonic.com>
  * @copyright Copyright (c) 2010 Brandon Kelly
  * @license   http://creativecommons.org/licenses/by-sa/3.0/ Attribution-Share Alike 3.0 Unported
+ *
+ * This is a conversion of Brandon's EE2 extension meant to be used with EE 1.x
  */
 
-class Wygwam_upload_tags_ext {
+class Wygwam_upload_tags {
 
 	var $name           = 'Wygwam Upload Tags';
 	var $version        = '1.0';
@@ -20,10 +21,10 @@ class Wygwam_upload_tags_ext {
 	/**
 	 * Class Constructor
 	 */
-	function Wygwam_upload_tags_ext()
+	function Wygwam_upload_tags()
 	{
 		// Make a local reference to the ExpressionEngine super object
-		$this->EE =& get_instance();
+		
 	}
 
 	// --------------------------------------------------------------------
@@ -33,15 +34,16 @@ class Wygwam_upload_tags_ext {
 	 */
 	function activate_extension()
 	{
+	   global $DB;
 		// add the row to exp_extensions
-		$this->EE->db->insert('extensions', array(
-			'class'    => 'Wygwam_upload_tags_ext',
+		$DB->query($DB->insert_string('exp_extensions', array(
+			'class'    => 'Wygwam_upload_tags',
 			'hook'     => 'wygwam_config',
 			'method'   => 'wygwam_config',
 			'priority' => 10,
 			'version'  => $this->version,
 			'enabled'  => 'y'
-		));
+		)));
 	}
 
 	/**
@@ -58,8 +60,10 @@ class Wygwam_upload_tags_ext {
 	 */
 	function disable_extension()
 	{
+	   global $DB;
+	   
 		// Remove all Wygwam_upload_tags_ext rows from exp_extensions
-		$this->EE->db->query('DELETE FROM exp_extensions WHERE class = "Wygwam_upload_tags_ext"');
+		$DB->query('DELETE FROM exp_extensions WHERE class = "Wygwam_upload_tags"');
 	}
 
 	// --------------------------------------------------------------------
@@ -69,30 +73,32 @@ class Wygwam_upload_tags_ext {
 	 */
 	function wygwam_config($config, $settings)
 	{
+	   global $EXT, $FNS, $SESS;
 		// If another extension shares the same hook,
 		// we need to get the latest and greatest config
-		if ($this->EE->extensions->last_call !== FALSE)
+		if ($EXT->last_call !== FALSE)
 		{
-			$config = $this->EE->extensions->last_call;
+			$config = $EXT->last_call;
 		}
-
+      
 		// Does this field have an Upload Directory set?
 		if ($settings['upload_dir'] && isset($config['filebrowserImageBrowseUrl']))
 		{
 			// Prepare the tags we wish to parse
 			$vars = array(
-				'username'  => $this->EE->session->userdata['username'],
-				'member_id' => $this->EE->session->userdata['member_id'],
-				'group_id'  => $this->EE->session->userdata['group_id']
+				'username'  => $SESS->userdata['username'],
+				'member_id' => $SESS->userdata['member_id'],
+				'group_id'  => $SESS->userdata['group_id']
 			);
 
 			// Get a reference to the Upload Directory session array
 			$sess =& $_SESSION['wygwam_'.$settings['upload_dir']];
 
 			// Parse the Server Path and URL settings
-			$sess['p'] = $this->EE->functions->var_swap($sess['p'], $vars); // Server Path
-			$sess['u'] = $this->EE->functions->var_swap($sess['u'], $vars); // URL
+			$sess['p'] = $FNS->var_swap($sess['p'], $vars); // Server Path
+			$sess['u'] = $FNS->var_swap($sess['u'], $vars); // URL
 		}
+      
 
 		// Return the (unmodified) config
 		return $config;
